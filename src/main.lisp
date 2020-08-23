427211c23521f8c2fb3467df4298f43648801b6d
Neil Munro <neilmunro@gmail.com>
2020-08-23 17:27:45 +0100

    Cleaned up the plain escape code

--- src/main.lisp
+++ src/main.lisp
@@ -5,4 +5,4 @@
 
 (defun cfmt (stream content &key (color cfmt.colors:none) (modifier cfmt.modifiers:none))
   (let ((formatter (format nil "[~A;~Am" modifier color)))
-    (format stream "~c~A~A~c~A" #\Esc formatter (format nil content) #\Esc "[0m")))
+    (format stream "~c~A~A~c~A" #\Esc formatter (format nil content) #\Esc (format nil "[~A;~Am" cfmt.colors:none cfmt.modifiers:none))))
