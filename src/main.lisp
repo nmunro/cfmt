ee9171aedb735e2aa2bd0d173fedb30007c8137a
Neil Munro <neilmunro@gmail.com>
2020-08-23 13:26:07 +0100

    Removed old test code

--- src/main.lisp
+++ src/main.lisp
@@ -6,9 +6,3 @@
 (defun cfmt (stream content &key (color cfmt.colors:none) (modifier cfmt.modifiers:none))
   (let ((formatter (format nil "[~A;~Am" modifier color)))
     (format stream "~c~A~A~c~A" #\Esc formatter (format nil content) #\Esc "[0m")))
-
-;(cfmt t "this should~%be green~%" :color green)
-;(cfmt t "this is normal text~%")
-;(cfmt t "This is a test~%" :color cyan)
-;(cfmt t "This is a test~%" :color black)
-;(cfmt t (format nil "Hello ~A, how are you?~%" (cfmt nil "NMunro" :color green :modifier underline)))
